6ef6ff75c8ae295e73f173e5142fd55a3de8ea6e
翼阳 <heyang.why@alibaba-inc.com>
2019-03-15 22:35:00 +0800

    finish 1 questions in 2019年03月15日22:34:58

---
+++ string/_438_FindAllAnagrams.java
@@ -0,0 +1,42 @@
+package leetcode.string;
+
+import java.util.*;
+
+/**
+ * @author baikal on 2019-03-15
+ * @project Algorithm
+ */
+public class _438_FindAllAnagrams {
+    public static List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        if (s.length() < p.length()) {
+            return res;
+        }
+
+        char[] c = p.toCharArray();
+        Arrays.sort(c);
+        String p1 = String.valueOf(c);
+        int n = p1.length();
+
+        for (int i = n - 1; i < s.length(); i++) {
+            int initIndex = i - (n - 1);
+            String s1 = s.substring(initIndex, initIndex + n);
+            char[] c2 = s1.toCharArray();
+            Arrays.sort(c2);
+            s1 = String.valueOf(c2);
+            if (s1.equals(p1)) {
+                res.add(initIndex);
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "cbaebabacd";
+        String p = "abc";
+        List<Integer> res = findAnagrams(s, p);
+
+        System.out.println(res);
+    }
+}
